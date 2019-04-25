678dd9dfa543dceb6a840ee411c4b26541feda10
Loren Oh <loh@eoas.ubc.ca>
2019-04-25 00:57:23 -0700

    Remove mirrored duplicates

--- src/compare_sol.py
+++ src/compare_sol.py
@@ -11,7 +11,7 @@ _cs = lib_nlp.cosine_sim
 w_, _ = lib_util.get_terminal_size()
 
 def compare_sol():
-    df = lib_csv.read_csv('../data/a1_q8.csv')
+    df = lib_csv.read_csv('../data/test.csv')
     df.columns = ['name', 'id', 'answer']
     
     scores = _cs(df.answer)
@@ -20,6 +20,13 @@ def compare_sol():
     # Ignore identity matrix and search
     scores[im_ == 1] = 0
     x, y = np.where(scores > 0.5)
+
+    # Remove mirrored pairs
+    _df = pd.DataFrame({'x':x, 'y':y})
+    _df = pd.DataFrame(np.sort(_df, axis=1), columns=['x', 'y'])
+    _df = lib_util.reset_index(_df.drop_duplicates())
+
+    x, y = _df.values.T
 
     for i in range(len(x)):
         print(f'Similiarity score: {scores[x[i], y[i]]:.3f}\n')
